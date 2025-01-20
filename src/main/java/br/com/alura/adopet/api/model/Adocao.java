34b213e86f5e7f0170a987aecabee9eade707a52
NelsonFilho01 <nelsoncamarafilho@gmail.com>
2025-01-20 17:10:51 -0300

    Refatorando oque esqueci

--- src/main/java/br/com/alura/adopet/api/model/Adocao.java
+++ src/main/java/br/com/alura/adopet/api/model/Adocao.java
@@ -1,7 +1,6 @@
 package br.com.alura.adopet.api.model;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 import jakarta.persistence.*;
 import java.time.LocalDateTime;
 import java.util.Objects;
